a3fd5992ccf15f00b08b7b1ca6df52955ec6b303
Maksaks <maksym.zhmutskyi@gmail.com>
2023-04-18 14:12:38 +0300

    Add domain layer with main entities

---
+++ Course_project_GYMAPP.Domain/Entity/Admin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course_project_GYMAPP.Domain.Entity
+{
+    internal class Admin
+    {
+    }
+}
---
+++ Course_project_GYMAPP.Domain/Entity/Trainer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course_project_GYMAPP.Domain.Entity
+{
+    public class Trainer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public int Age { get; set; }
+        public string Number { get; set; }
+        public string ImgPath { get; set; }
+        public string AboutInfo { get; set; }
+        public DateTime DateReg  { get; set; }
+    }
+}
