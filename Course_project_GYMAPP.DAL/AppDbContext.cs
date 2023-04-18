c49932eada295fbfa20a4b3741dc1c2cf5d32c83
Maksaks <maksym.zhmutskyi@gmail.com>
2023-04-18 14:46:45 +0300

    Add new layer "DAL" with DbContext class and set connection to DB

---
+++ Course_project_GYMAPP.DAL/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Course_project_GYMAPP.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course_project_GYMAPP.DAL
+{
+    public class AppDbContext : DbContext
+    {
+
+        public DbSet<User>
+
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { 
+            Database.EnsureCreated();
+        }
+    }
+}
